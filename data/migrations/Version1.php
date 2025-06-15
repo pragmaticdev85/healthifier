68a3b23792e3818efa7ad436d0db11c37ee7a05e
Shanmukha K <shanmukha.k@gmail.com>
2025-06-15 17:40:33 +0530

    DB Script stabilised

--- data/migrations/Version1.php
+++ data/migrations/Version1.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250607131200 extends AbstractMigration
+final class Version1 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -19,7 +19,7 @@ final class Version20250607131200 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql("" . readfile(__DIR__ . '/DDL_v2_0.sql') . "");
+        $this->addSql(file_get_contents(__DIR__ . '/DDL_v1_0.sql'));
     }
 
     public function down(Schema $schema): void
---
+++ data/migrations/Version2.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace initial_setup;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version2 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(file_get_contents(__DIR__ . '/DDL_v2_0.sql'));
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
